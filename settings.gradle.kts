24622e0e96b3eb60853956786b86d71a96972d81
tamamul <36046825+tamamul@users.noreply.github.com>
2025-09-17 15:27:03 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,25 @@
 pluginManagement {
     repositories {
-        val localProperties = java.util.Properties()
-        localProperties.load(java.io.FileInputStream(file("local.properties")))
+        google()
+        mavenCentral()
+        gradlePluginPortal()
 
-        maven {
-            url = uri("https://inexus.samentic.com/repository/samentic-android/")
-            credentials {
-                username = localProperties["nexus.username"] as? String
-                password = localProperties["nexus.password"] as? String
+        // repo nexus custom
+        val localProperties = java.util.Properties()
+        val localFile = file("local.properties")
+        if (localFile.exists()) {
+            localProperties.load(java.io.FileInputStream(localFile))
+
+            maven {
+                url = uri("https://inexus.samentic.com/repository/samentic-android/")
+                credentials {
+                    username = localProperties["nexus.username"] as? String
+                    password = localProperties["nexus.password"] as? String
+                }
             }
         }
+
+        // jitpack
         maven { url = uri("https://jitpack.io") }
     }
 }
